fb3b280ec1a3d529f4ed8fba4d356268bb513060
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-08 20:05:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
     $(".command-action").click(function() {
-      var textElement = $(this).find(".command-action-text");
-      var shevronIcon = $("#chevron-icon");
-      var commandList = $(".command-list");
-  
-      if (textElement.text() === "показать") {
-        textElement.text("скрыть");
-        shevronIcon.css("transform", "rotate(180deg)");
-        commandList.addClass("expanded");
-      } else {
-        textElement.text("показать");
-        shevronIcon.css("transform", "rotate(0deg)");
-        commandList.removeClass("expanded");
-      }
+        var textElement = $(this).find(".command-action-text");
+        var shevronIcon = $("#chevron-icon");
+        var commandList = $(".command-list");
+
+        if ($(event.target).closest('.command-block').length === 0) {
+            $(".command-list").toggleClass("expanded");
+        }
+        
+        if (textElement.text() === "показать") {
+            textElement.text("скрыть");
+            shevronIcon.css("transform", "rotate(180deg)");
+            commandList.addClass("expanded");
+        }
+
+        else {
+            textElement.text("показать");
+            shevronIcon.css("transform", "rotate(0deg)");
+            commandList.removeClass("expanded");
+        }
     });
-  });
-  
+
+    $(".command-block").click(function() {
+        var link = $(this).data("link");
+        window.location.href = link;
+    });
+});
