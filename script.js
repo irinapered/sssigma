1a5f234806a24497fa12eba26803b27a20a9e06d
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-08 19:09:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-    $(".commands").click(function() {
-      $(this).parent().find(".command-details").toggleClass("show-menu");
+    $(".command-action").click(function() {
+      var textElement = $(this).find(".command-action-text");
+      var shevronIcon = $("#chevron-icon");
+      var commandList = $(".command-list");
+  
+      if (textElement.text() === "показать") {
+        textElement.text("скрыть");
+        shevronIcon.css("transform", "rotate(180deg)");
+        commandList.addClass("expanded");
+      } else {
+        textElement.text("показать");
+        shevronIcon.css("transform", "rotate(0deg)");
+        commandList.removeClass("expanded");
+      }
     });
   });
   
