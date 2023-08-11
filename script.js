6d7ec58d17c9f0ab234787314f143da00e1e8e61
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-11 03:25:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-$(document).ready(function() {
-    $(".command-action").click(function() {
-        var textElement = $(this).find(".command-action-text");
-        var shevronIcon = $("#chevron-icon");
-        var commandList = $(".command-list");
-
-        if ($(event.target).closest('.command-block-url').length === 0) {
-            $(".command-list").toggleClass("expanded");
-        }
-        
-        if (textElement.text() === "показать") {
-            textElement.text("скрыть");
-            shevronIcon.css("transform", "rotate(180deg)");
-            commandList.addClass("expanded");
-        }
-
-        else {
-            textElement.text("показать");
-            shevronIcon.css("transform", "rotate(0deg)");
-            commandList.removeClass("expanded");
-        }
-    });
-
-    $(".command-block-url").click(function() {
-        var link = $(this).data("link");
-        window.location.href = link;
-    });
-});
+function navigate() {
+    const block = document.querySelector('.block');
+    const container = document.querySelector('.container');
+  
+    block.style.animation = 'fade-out 0.5s forwards';
+    container.style.animation = 'shift-left 0.5s forwards';
+  
+    setTimeout(() => {
+      window.location.href = 'новая_страница.html';
+    }, 500);
+  }
+  
