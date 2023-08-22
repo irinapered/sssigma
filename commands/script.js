e66ba5fd4c5ffdb5df4a34289c87a78f57ba0154
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-22 15:47:57 +0300

    Add files via upload

---
+++ commands/script.js
@@ -0,0 +1,10 @@
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
+
+tg.expand(); //расширяем на все окно
+
+tg.MainButton.show()
+tg.MainButton.setText("Назад");
+
+tg.BackButton.show()
+tg.impactOccurred(rigid)
+tg.setHeaderColor("#CD5152")
