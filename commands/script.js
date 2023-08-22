0aa01fb23241f87adc589749d5bc0ffbcbefc232
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-22 17:26:26 +0300

    Update script.js

--- commands/script.js
+++ commands/script.js
@@ -2,9 +2,9 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 
 tg.expand(); //расширяем на все окно
 
+tg.MainButton.show()
 tg.MainButton.setText("Назад");
 tg.MainButton.Text("JJJJJ");
-tg.MainButton.show()
 
 tg.BackButton.show()
 tg.HapticFeedback.impactOccurred('rigid')
