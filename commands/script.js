ecfe7462b8b0dad0d5f18cd9750b1bf02f5f2abd
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-22 17:37:08 +0300

    Update script.js

--- commands/script.js
+++ commands/script.js
@@ -4,10 +4,7 @@ tg.expand(); //расширяем на все окно
 
 tg.MainButton.show()
 tg.MainButton.setText("Назад");
-tg.MainButton.Text("JJJJJ");
 
 tg.BackButton.show()
-tg.HapticFeedback.impactOccurred('rigid')
 tg.HapticFeedback.impactOccurred(rigid)
-Telegram.WebApp.HapticFeedback.impactOccurred('heavy')
-tg.setHeaderColor('secondary_bg_color')
+tg.setHeaderColor("#CD5152")
