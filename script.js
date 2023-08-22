1302d62e6f8db8a73af944524e762861e45cecee
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-22 15:31:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
 
+tg.expand(); //расширяем на все окно
+
+function setWebsiteBackgroundColor(colorScheme, navigationBars) {
+    const body = document.body;
+
+    if (colorScheme === 'dark') {
+        body.style.backgroundColor = 'black';
+        navigationBars.forEach(navigationBar => {
+            navigationBar.style.backgroundColor = '#1C1B1E';
+        });
+    } else {
+        body.style.backgroundColor = '#F2F1F6';
+
+    }
+}
+
+function setThemeClass() {
+    const colorScheme = Telegram.WebApp.colorScheme;
+    const navigationBars = document.querySelectorAll('.navigationBar');
+    setWebsiteBackgroundColor(colorScheme, navigationBars);
+}
+
+Telegram.WebApp.onEvent('themeChanged', setThemeClass);
+setThemeClass();
+
+
+
+
+function openPage(pageURL) {
+    const body = document.body;
+    body.style.transform = 'translateX(-200px)';
+    
+    setTimeout(() => {
+      window.location.href = pageURL;
+    }, 1000);
+  }
