e696502075c8d55cc8d11e330aa065dd370d5c14
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-25 07:24:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,7 @@ tg.expand(); //расширяем на все окно
 // tg.MainButton.show();
 // tg.MainButton.setText("Закрыть навигацию");
 
-function setWebsiteBackgroundColor(colorScheme, navigationBars) {
-    const body = document.body;
 
-    if (colorScheme === 'dark') {
-        body.style.backgroundColor = 'black';
-        navigationBars.forEach(navigationBar => {
-            navigationBar.style.backgroundColor = '#1C1B1E';
-        });
-    } else {
-        body.style.backgroundColor = '#F2F1F6';
-
-    }
-}
-
-function setThemeClass() {
-    const colorScheme = Telegram.WebApp.colorScheme;
-    const navigationBars = document.querySelectorAll('.navigationBar');
-    setWebsiteBackgroundColor(colorScheme, navigationBars);
-}
-
-Telegram.WebApp.onEvent('themeChanged', setThemeClass);
-setThemeClass();
 
 
 
