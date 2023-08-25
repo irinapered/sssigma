b9dcd363de3816aad53339c4a50a9c144137d952
irinapered <141699004+irinapered@users.noreply.github.com>
2023-08-25 18:57:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
 
 tg.expand(); //расширяем на все окно
-window.Telegram.WebApp.expand();
+tg.MainButton.setText("Закрыть навигацию")
 
 // tg.MainButton.show();
 // tg.MainButton.setText("Закрыть навигацию");
